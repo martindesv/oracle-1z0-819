e6f4724c93204cb1fca8d7b3c946b6763311e28c
Martin A <martin.apalkov@wisercat.eu>
2021-12-13 14:07:05 +0200

    classes

---
+++ src/Bat.java
@@ -0,0 +1,15 @@
+package src;
+
+class Mammal {
+    String type = "mammal";
+}
+public class Bat extends Mammal {
+    String type = "bat";
+    public String getType() {
+        return super.type + ":" + this.type;
+    }
+    public static void main(String... zoo) {
+        System.out.println(new Bat().getType());
+        System.out.print(new Bat().type);
+    }
+}
