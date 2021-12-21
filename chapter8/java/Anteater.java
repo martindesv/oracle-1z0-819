7a77747935aba3fdb1a6e0f14b3de474cbb377af
Martin A <martin.apalkov@gmail.com>
2021-12-21 21:56:13 +0200

    classes

---
+++ chapter8/java/Anteater.java
@@ -0,0 +1,7 @@
+package chapter8.java;
+
+import java.util.ArrayList;
+
+public class Anteater extends LongTailAnimal {
+    protected void chew(ArrayList<Double> input) {}
+}
---
+++ chapter8/java/LongTailAnimal.java
@@ -0,0 +1,8 @@
+package chapter8.java;
+
+import java.util.List;
+
+public class LongTailAnimal {
+    protected void chew(List<Object> input) {}
+}
+
